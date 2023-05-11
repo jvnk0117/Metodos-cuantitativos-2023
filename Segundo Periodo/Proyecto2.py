3bf00b7a4abdc756348a590a915a10b3cde854e8
Perez Gonzalez <amalgonz@amazon.com>
2023-05-11 05:17:59 -0600

    1st half created

--- Segundo Periodo/Proyecto2.py
+++ Segundo Periodo/Proyecto2.py
@@ -1,29 +1,41 @@
 #Loading resources
-from random import randint as r
+import random as r
 from prettytable import PrettyTable
+import datetime as d
 
-n = int(input("Enter the number of clients: \n"))
-#Client numbers with arrivals 
+#Defining number of 
+n = int(input("Enter the number of clients: "))
+
+
 client = []
 timeBetweenArrivals = []
 arrivalsTime = []
+transactionTime = []
 
+timeBetweenArrivals.append(0)
 
 for i in range(1,n+1):
     client.append(i)
+for j in range(1,n):
+    timeBetweenArrivals.append(r.randint(1,10))
 
-print(client)
+fixedHour = r.randint(0,1380)
+arrivalsTime.append(str(d.timedelta(minutes = fixedHour)))
+
+for k in range(1,n):
+    fixedHour += timeBetweenArrivals[k]
+    arrivalsTime.append(str(d.timedelta(minutes = fixedHour)))
 
 
+for j in range(0,n):
+    transactionTime.append(r.randint(1,5))
+    
+
+runs_table = PrettyTable()
+runs_table.add_column('Cliente', client)
+runs_table.add_column('Tiempo entre llegadas', timeBetweenArrivals)
+runs_table.add_column('Hora de llegada', arrivalsTime)
+runs_table.add_column('Tiempo del tramite', transactionTime)
 
 
-
-
-
-
-
-# runs_table = PrettyTable()
-# runs_table.add_column("Statistics",["Miu","Sigma","Zscore",""])
-# runs_table.add_column("Statistics",["Miu","Sigma","Zscore","rerw"])
-
-# print(runs_table)
+print(runs_table)
